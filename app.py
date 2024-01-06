4838925f0dbd0ffa2aa92213c9533c079d817b7b
BigBobLittle <littletheprogrammer@gmail.com>
2024-01-06 17:13:30 +0000

    feat: added option to read multiple pdf files and write the text to console

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st 
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+load_dotenv()
+
+def get_pdf_text(pdfs):
+    text = ''
+    # loop through each pdf provided and set it to a variable pdf_reader
+    for each_pdf in pdfs:
+        pdf_reader = PdfReader(each_pdf)
+        # loop through each single page for each of the pdf documents, extract the text and return it
+        for each_pdf_doc in pdf_reader.pages:
+            text+= each_pdf_doc.extract_text()
+    return text
+            
 
 def main():
     st.set_page_config(page_title='Chat With Your PDF', page_icon=":books:")
@@ -8,8 +23,13 @@ def main():
     # set a sidebar and put things in it 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your pdf here and click on process")
-        st.button("Process")
+        pdfs = st.file_uploader("Upload your pdf here and click on process", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                
+                raw_text = get_pdf_text(pdfs)
+                st.write(raw_text)
+                # st.success("Done processing ")
         
 
 
