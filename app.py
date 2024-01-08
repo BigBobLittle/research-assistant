2b5df6dbb60436700ac20fdcc686fa467e719404
BigBobLittle <littletheprogrammer@gmail.com>
2024-01-08 18:43:00 +0000

    feat: added simple comments

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ from langchain_community.chat_models import ChatOpenAI
 import pickle 
 import os
 
-
+# load env for custom api keys
 load_dotenv()
 
+# receives any number of pdfs, reach each one of them and extract the text
 def get_pdf_text(pdfs):
     text = ''
-    # loop through each pdf provided and set it to a variable pdf_reader
+    
     for each_pdf in pdfs:
         pdf_reader = PdfReader(each_pdf)
-        # loop through each single page for each of the pdf documents, extract the text and return it
+        
         for each_pdf_doc in pdf_reader.pages:
             text+= each_pdf_doc.extract_text()
     return text
@@ -32,21 +33,25 @@ def get_text_chunks(text):
     return text_splitter.split_text(text)
 
 
+# create a FAISS vector store, using openapi embeddings 
 def openai_vector_store(text_chunks):
     embeddings = OpenAIEmbeddings()
     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vector_store
 
+# create a vector store with hugging face instructor-xl model, -quiet slow
 def huggingface_vector_store(texts):
     embedding = HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-xl')
     return FAISS.from_texts(texts, embedding)
 
+# use langchain to create a conversation chain, with conversation history
 def create_conversation_chain(vector_store):
     llm= ChatOpenAI()
     memory = ConversationBufferMemory(memory_key='chat_history',  return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, memory=memory, retriever=vector_store.as_retriever())
     return conversation_chain
 
+# when a user asks a question, display the user question and answer after it's processed
 def handle_user_question(prompt):
     pdfs = st.session_state.files
     if(len(pdfs) < 1):
@@ -65,8 +70,9 @@ def handle_user_question(prompt):
                  ai_assistant =  st.chat_message('AI')
                 #  ai_assistant.markdown(message.content)
                  ai_assistant.markdown(f"<div class='ai-msg'>{message.content}</div>", unsafe_allow_html=True)
-        
 
+
+# markdown to format the chat ui interface
 def markdown():
     return st.markdown(
         """
@@ -97,6 +103,7 @@ def markdown():
         unsafe_allow_html=True
     )
 
+# check if a pickle file of a document already exists, don't recreate it's embeddings 
 def check_and_load_pickle_files(pdfs):
     pickle_data = {}
 
@@ -109,7 +116,7 @@ def check_and_load_pickle_files(pdfs):
                 pickle_data[each_pdf] = pickle.load(file)
     return pickle_data
 
-
+# main function of application, set the page config, sidebar and chat section of the app
 def main():
     st.set_page_config(page_title='Chat With Your PDF', page_icon=":books:")
     st.header("Chat With Multiple PDFs")
